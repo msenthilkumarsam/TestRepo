8caa2dd4d559c89570b3f99e64ce271b545143d2
msenthilkumarsam <msenthilkumarsam@gmail.com>
2017-12-21 17:35:13 +0530

    SampleTest

---
+++ app-java/src/main/java/TestMe.java
@@ -0,0 +1,9 @@
+
+public class TestMe {
+
+	public static void main(String[] args) {
+		HelloWorld hw=new HelloWorld();
+		hw.coveredByIntegrationTest();
+	}
+
+}
---
+++ app-java/src/test/java/HelloWorldUnitTest.java
@@ -0,0 +1,12 @@
+
+
+import org.junit.Test;
+
+public class HelloWorldUnitTest {
+
+  @Test
+  public void test() {
+    new HelloWorld().coveredByUnitTest();
+  }
+
+}
